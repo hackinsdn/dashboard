e8ce78d0fb5c8f99a57177b3b617677e1df9e2f2
Italo Valcy <italo@ampath.net>
2025-01-10 13:30:03 -0300

    run flask with threads

--- run-flask.sh
+++ run-flask.sh
@@ -10,5 +10,5 @@ export FLASK_APP=run.py
 if [ ! -f key.pem ]; then
 	openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=aw-sdx-meican.amlight.net"
 fi
-flask run --port 5000 --host 127.0.0.1
+flask run --with-threads --port 5000 --host 127.0.0.1
 #flask run --port 443 --host 0.0.0.0 --cert cert.pem --key key.pem
