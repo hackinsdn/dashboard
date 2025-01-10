a707b810344b1e760aaaf7f9d88f28627b0246de
Italo Valcy <italo@ampath.net>
2025-01-10 14:28:46 -0300

    simplify run.py

--- run.py
+++ run.py
@@ -1,22 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-import os
 from flask_migrate import Migrate
 
 from apps.config import app_config
 from apps import create_app, db
 
-# WARNING: Don't run with debug turned on in production!
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
-
 app = create_app(app_config)
 Migrate(app, db)
-
-if DEBUG:
-    app.logger.info('DEBUG            = ' + str(DEBUG)             )
-    app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
-    app.logger.info('DBMS             = ' + app_config.SQLALCHEMY_DATABASE_URI)
-    app.logger.info('ASSETS_ROOT      = ' + app_config.ASSETS_ROOT )
 
 if __name__ == "__main__":
     app.run()
