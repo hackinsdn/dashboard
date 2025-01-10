ff86b744355ef87515bd853351e16c11ff22b426
Italo Valcy <italo@ampath.net>
2025-01-10 06:04:48 -0300

    refactor run-flask and env files

--- run-flask.sh
+++ run-flask.sh
@@ -2,16 +2,8 @@
 
 source venv/bin/activate
 
-export OAUTH_CLIENT_ID=xxx
-export OAUTH_CLIENT_SECRET=xxx
-export OAUTH_DOMAIN=cilogon.org
-#export OAUTH_CLIENT_ID=APP-xx
-#export OAUTH_CLIENT_SECRET=xxx
-#export OAUTH_DOMAIN=orcid.org
+source .env
 
-export K8S_NAMESPACE=hackinsdn
-export SECRET_KEY=xxx
-export FLASK_ENV=development
 export FLASK_APP=run.py
 if [ ! -f key.pem ]; then
 	openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=aw-sdx-meican.amlight.net"
