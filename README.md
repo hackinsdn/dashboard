0a24e2f9c1375a9ba1bb780653f837e5a1f28b83
Italo Valcy <italo@ampath.net>
2025-01-10 09:50:19 -0300

    refactor the run procedure and documenting

--- README.md
+++ README.md
@@ -10,4 +10,9 @@ cd dashboard
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt
+deactivate
+cp env-template .env
+# ==> change .env file as needed
+# ==> you may also want to change apps/config.py
+./run-flask.sh
 ```
--- run-flask.sh
+++ run-flask.sh
@@ -4,7 +4,9 @@ source venv/bin/activate
 
 source .env
 
+export FLASK_DEBUG=$DEBUG
 export FLASK_APP=run.py
+
 if [ ! -f key.pem ]; then
 	openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=aw-sdx-meican.amlight.net"
 fi
