3ce63ca7612819fcb35dc538c616fd0dfdd0c4e3
beatrizx2 <beaazevedo14@gmail.com>
2025-01-17 14:44:15 -0300

    removes flask mail

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 # -*- encoding: utf-8 -*-
 
 from flask_migrate import Migrate
-from flask_mail import Mail
 from apps.config import app_config
 from apps import create_app, db
 
 app = create_app(app_config)
 Migrate(app, db)
-mail = Mail(app) # Initializing Flask-Mail with the app
 
 if __name__ == "__main__":
     app.run()
