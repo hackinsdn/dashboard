5d60ebb469f0240d5298acfa63a8220b4a43e7a0
Italo Valcy <italovalcy@gmail.com>
2025-04-16 04:35:26 -0300

    fix error when filtering running labs by group

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -43,10 +43,14 @@ def index():
 @login_required
 @check_user_category(["admin", "teacher", "student"])
 def running_labs():
-
     filter_group = request.args.get("filter_group", "")
+    filter_members = {}
     if filter_group.isdigit():
         filter_group = int(filter_group)
+        group = Groups.query.get(filter_group)
+        if not group or group.is_deleted:
+            return render_template("pages/error.html", title="Error getting running labs", msg="Group not found")
+        filter_members = group.members_dict
 
     registered_labs = {}
     allowed_groups_by_lab = {}
@@ -59,12 +63,17 @@ def running_labs():
         registered_user = {user.id: user for user in Users.query.filter_by(is_deleted=False).all()}
 
     current_user_groups = {}
+    current_user_priv_groups = {}
     for group in current_user.member_of_groups:
+        if group.organization == "SYSTEM":
+            continue
         current_user_groups[group.id] = group
     for group in current_user.assistant_of_groups:
         current_user_groups[group.id] = group
+        current_user_priv_groups[group.id] = group
     for group in current_user.owner_of_groups:
         current_user_groups[group.id] = group
+        current_user_priv_groups[group.id] = group
 
     lab_instances = LabInstances.query.filter_by(is_deleted=False)
     if not filter_group:
@@ -72,22 +81,18 @@ def running_labs():
 
     labs = []
     for li in lab_instances.all():
-        show_labinst = False
-        current_app.logger.info(f"filter lab_instances {filter_group=} {current_user_groups=} {allowed_groups_by_lab[li.lab_id]=}")
-        if filter_group == "all":
-            if current_user.category == "admin":
-                show_labinst = True
-            else:
-                for group_id in current_user_groups:
-                    if group_id in allowed_groups_by_lab[li.lab_id]:
-                        show_labinst = True
-        elif filter_group:
-            if filter_group in allowed_groups_by_lab[li.lab_id]:
-                show_labinst = True
+        is_allowed = False
+        # check if user has permission to see the lab
+        if current_user.category == "admin" or li.user_id == current_user.id:
+            is_allowed = True
         else:
-            if li.user_id == current_user.id:
-                show_labinst = True
-        if not show_labinst:
+            for group_id in current_user_priv_groups:
+                if group_id in allowed_groups_by_lab[li.lab_id]:
+                    is_allowed = True
+                    break
+        if not is_allowed:
+            continue
+        if filter_group != "all" and filter_group and li.user_id not in filter_members:
             continue
 
         user = registered_user.get(li.user_id)
