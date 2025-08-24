8085e86833385881a996ab0a8c6a3ac02f02ff62
Italo Valcy <italovalcy@gmail.com>
2025-08-24 06:33:28 -0300

    reduce queries to DB for lab_instances

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -466,9 +466,14 @@ def view_labs(lab_id=None):
         labs = labs.filter(Labs.id == lab_id)
     
     labs = labs.all()
-    user_lab_instances = LabInstances.query.filter_by(user_id=current_user.id).all()
-    completed_labs = {lab.lab_id for lab in LabInstances.query.filter_by(user_id=current_user.id, is_deleted=True).filter(LabInstances.finish_reason.isnot(None)).all()}
-    return render_template("pages/labs_view.html", labs=labs, lab_categories=lab_categories, user_lab_instances=user_lab_instances, completed_labs=completed_labs, segment="/labs/view")
+    user_labs_status = {}
+    for lab in LabInstances.query.filter_by(user_id=current_user.id).all():
+        user_labs_status.setdefault(lab.lab_id, {"is_running": False, "is_completed": False})
+        if not lab.is_deleted:
+            user_labs_status[lab.lab_id]["is_running"] = True
+        if lab.is_deleted and lab.finish_reason is not None:
+            user_labs_status[lab.lab_id]["is_completed"] = True
+    return render_template("pages/labs_view.html", labs=labs, lab_categories=lab_categories, user_labs_status=user_labs_status, segment="/labs/view")
 
 
 @blueprint.route('/groups/list')
