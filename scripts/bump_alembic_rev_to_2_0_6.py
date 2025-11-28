43a59080039e2a96ec99eb533fc518086f82c763
Italo Valcy <italovalcy@gmail.com>
2025-11-28 05:57:40 -0300

    Revert ":sparkles: feat: models Clab/ClabInstance"
    This reverts commit df65d39361df9f1596724ea9500b96f858d06d27.

--- scripts/bump_alembic_rev_to_2_0_6.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-p = "apps/data/db.sqlite3"
-con = sqlite3.connect(p)
-cur = con.cursor()
-cur.execute("update alembic_version set version_num='2.0.6'")
-con.commit()
-print("after=", cur.execute("select version_num from alembic_version").fetchall())
-con.close()
