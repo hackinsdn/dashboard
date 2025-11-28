4ff4bdb5e02304c3e38dc9c983c90d0c98e748ee
Italo Valcy <italovalcy@gmail.com>
2025-11-28 15:59:59 -0300

    fix statesData and globe.js on Leafmap

--- apps/config.py
+++ apps/config.py
@@ -116,7 +116,7 @@ class Config(object):
     TESTBED_TITLE = os.getenv("TESTBED_TITLE", "HackInSDN Testbed")
 
     # Map configuration
-    MAP_GEOJSON_JS = os.getenv("MAP_GEOJSON_JS", "globe.js")
+    MAP_GEOJSON_JS = os.getenv("MAP_GEOJSON_JS", "")
     MAP_CENTER_LAT = float(os.getenv('MAP_CENTER_LAT', "0"))
     MAP_CENTER_LNG = float(os.getenv('MAP_CENTER_LNG', "0"))
     MAP_ZOOM_LEVEL = int(os.getenv('MAP_ZOOM_LEVEL', "1"))
