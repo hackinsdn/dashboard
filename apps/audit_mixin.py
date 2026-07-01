67d3e8ecf5ecc1cdb15051fbdfb2e6f08c796e9c
Italo Valcy <italovalcy@gmail.com>
2026-07-01 13:59:15 +0000

    document how updated_by is being populated and used as created_by

--- apps/audit_mixin.py
+++ apps/audit_mixin.py
@@ -26,7 +26,9 @@ def get_remote_addr():
 class AuditMixin(object):
     created_at = Column(DateTime, default=utcnow)
     updated_at = Column(DateTime, default=utcnow, onupdate=utcnow)
-    # XXX: this column is actually being used as a created_by
+    # XXX: this column is actually being used as a created_by, the way it
+    # works is by leveraging the default value as being get_user_id() which
+    # get the current_user.id and inserts into the DB entry being inserted
     updated_by = Column(Integer, ForeignKey("users.id"), default=get_user_id)
 
 def check_user_category(allowed_categories):
